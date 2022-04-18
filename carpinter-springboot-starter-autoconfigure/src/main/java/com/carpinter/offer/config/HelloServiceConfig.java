4ce3b7c2e214d4441e6e34d1b641c5904153d6cd
xiaoning <carpinter_ning@163.com>
2022-04-19 01:40:11 +0800

    fix

--- carpinter-springboot-starter-autoconfigure/src/main/java/com/carpinter/offer/config/HelloServiceConfig.java
+++ carpinter-springboot-starter-autoconfigure/src/main/java/com/carpinter/offer/config/HelloServiceConfig.java
@@ -2,6 +2,7 @@ package com.carpinter.offer.config;
 
 import com.carpinter.offer.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @date 2020/5/26
  */
 @Configuration
+@EnableConfigurationProperties(HelloProperties.class)
 public class HelloServiceConfig {
 
     @Autowired
